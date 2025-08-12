d90b334a121b7304c5497492264846c736a08c90
Harsh Thorat <harshthorat18@gmail.com>
2025-08-12 23:50:57 +0530

    Update MagneticButton.tsx

--- components/effects/MagneticButton.tsx
+++ components/effects/MagneticButton.tsx
@@ -5,24 +5,28 @@ function lerp(start: number, end: number, t: number) {
   return start * (1 - t) + end * t;
 }
 
+type MagneticButtonOptions = {
+  duration?: number;
+  ease?: string | gsap.EaseFunction;
+};
+
 export function MagneticButton(
   element: string,
-  opts: { duration?: number; ease?: typeof Power2.easeOut } = {}
+  opts: MagneticButtonOptions = {}
 ) {
   document.querySelectorAll<HTMLElement>(element).forEach((elem) => {
     elem.addEventListener("mousemove", (e) => {
       const rect = elem.getBoundingClientRect();
-
       const zeroOneX = gsap.utils.mapRange(0, rect.width, 0, 1, e.clientX - rect.left);
       const zeroOneY = gsap.utils.mapRange(0, rect.height, 0, 1, e.clientY - rect.top);
-
+      
       // Grow mouse follower
       gsap.to(".mousefollower", {
         scale: 4,
         ease: Power2.easeOut,
         duration: 0.5,
       });
-
+      
       // Move element toward cursor
       gsap.to(elem, {
         x: lerp(-20, 20, zeroOneX),
@@ -31,7 +35,7 @@ export function MagneticButton(
         ease: opts.ease || Expo.easeOut,
       });
     });
-
+    
     elem.addEventListener("mouseleave", () => {
       // Reset mouse follower
       gsap.to(".mousefollower", {
@@ -39,7 +43,7 @@ export function MagneticButton(
         ease: Power2.easeOut,
         duration: 0.5,
       });
-
+      
       // Reset element position
       gsap.to(elem, {
         x: 0,
